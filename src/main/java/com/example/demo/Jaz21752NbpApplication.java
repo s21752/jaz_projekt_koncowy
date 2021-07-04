42195c6c40ff1e4115b413289b2ebbe473785188
Cezary <cezarygraban@gmail.com>
2021-07-04 14:15:23 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/Jaz21752NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaz21752NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jaz21752NbpApplication.class, args);
+    }
+
+}
