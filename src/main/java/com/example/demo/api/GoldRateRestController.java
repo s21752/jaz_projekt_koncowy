a48bb64dc008bfed1499e4e6ff1f930f9b4d313b
Cezary <cezarygraban@gmail.com>
2021-07-04 16:13:33 +0200

    Added get endpoint swagger documentation

--- src/main/java/com/example/demo/api/GoldRateRestController.java
+++ src/main/java/com/example/demo/api/GoldRateRestController.java
@@ -1,6 +1,8 @@
 package com.example.demo.api;
 
 import com.example.demo.data.source.service.GoldRateService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +21,13 @@ public class GoldRateRestController {
         this.goldRateService = goldRateService;
     }
 
+    @ApiOperation(
+            value = "Get average rate of gold prices",
+            response = String.class,
+            notes = "Returns average gold rate for specified date range (or for last month if no range specified)"
+    )
     @GetMapping
-    public ResponseEntity<String> returnAverageRate(@RequestParam Optional<String> since, @RequestParam Optional<String> upTo) {
+    public ResponseEntity<String> returnAverageRate(@ApiParam(name = "sinceWhen", example = "21-07-2020", value = "Date since when you want to check average gold rate") @RequestParam Optional<String> since, @ApiParam(name = "upToWhen", example = "25-07-2020", value = "Date up to when you want to check average gold rate") @RequestParam Optional<String> upTo) {
 
         var goldRateString = goldRateService.getGoldRate(since, upTo);
 
